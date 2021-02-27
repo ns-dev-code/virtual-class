f8fc50eb399088f6c8bf284d59743e14f5f9a366
nitinsingh <nitinsingh@digiryte.com>
2021-02-27 18:48:13 +0530

    Config modified

--- src/config/index.js
+++ src/config/index.js
@@ -1,43 +1,21 @@
 const API_KEY = process.env.FIREBASE_API_KEY;
 
 const secrets = () => {
-  if (process.env.NODE_ENV !== "production") {
-    const firebaseConfig = {
-      apiKey: "AIzaSyCQHOmeuT8y_x2QYhU96SAagHDDUAPuzT0",
-      authDomain: "mydemo-7570.firebaseapp.com",
-      databaseURL: "https://mydemo-7570.firebaseio.com",
-      projectId: "mydemo-7570",
-      storageBucket: "mydemo-7570.appspot.com",
-      messagingSenderId: "396911617749"
-    };
-    // http://localhost:5002
-    // https://us-central1-talentexcel-1.cloudfunctions.net/api
-    const cloudApi = "https://us-central1-talentexcel-1.cloudfunctions.net/api";
 
-    return {
-      cloudApi,
-      firebaseConfig
-    };
-  } else {
+  const firebaseConfig = {
+    apiKey: "AIzaSyCQHOmeuT8y_x2QYhU96SAagHDDUAPuzT0",
+    authDomain: "mydemo-7570.firebaseapp.com",
+    databaseURL: "https://mydemo-7570.firebaseio.com",
+    projectId: "mydemo-7570",
+    storageBucket: "mydemo-7570.appspot.com",
+    messagingSenderId: "396911617749"
+  };
+  const cloudApi = "";
 
-    const firebaseConfig = {
-      apiKey: "AIzaSyCQHOmeuT8y_x2QYhU96SAagHDDUAPuzT0",
-      authDomain: "mydemo-7570.firebaseapp.com",
-      databaseURL: "https://mydemo-7570.firebaseio.com",
-      projectId: "mydemo-7570",
-      storageBucket: "mydemo-7570.appspot.com",
-      messagingSenderId: "396911617749"
-    };
-    // http://localhost:5002
-    // https://us-central1-talentexcel-1.cloudfunctions.net/api
-    //const cloudApi = "https://us-central1-orgz-app.cloudfunctions.net/api";
-    const cloudApi = "https://us-central1-orgz-app.cloudfunctions.net/api";
-
-    return {
-      firebaseConfig,
-      cloudApi
-    };
-  }
+  return {
+    cloudApi,
+    firebaseConfig
+  };
 };
 
 const { firebaseConfig, cloudApi } = secrets();
