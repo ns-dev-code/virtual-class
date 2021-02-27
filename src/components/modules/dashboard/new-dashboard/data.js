9a77c635f3d4903261742b0c1a1a47f79265884b
nitinsingh <nitinsingh@digiryte.com>
2021-02-27 19:33:05 +0530

    UUID removed

--- src/components/modules/dashboard/new-dashboard/data.js
+++ src/components/modules/dashboard/new-dashboard/data.js
@@ -1,25 +1,23 @@
-import uuid from 'uuid/v4'
-import { truncate } from 'lodash'
 
 export const data = [
     {
         title: 'Session #1',
         date: new Date(),
-        code: uuid()
+        code: '9 BE 4 BEA 4'
     },
     {
         title: 'Session #2',
         date: new Date(),
-        code: uuid()
+        code: '3 D 5 AA 439'
     },
     {
         title: 'Session #3',
         date: new Date(),
-        code: uuid()
+        code: 'BFBDCA 7 C'
     },
     {
         title: 'Session #4',
         date: new Date(),
-        code: uuid()
+        code: 'E 3090667'
     }
 ]
